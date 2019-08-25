8d230f5b21baf5d042a118590b797a956149b424
gmeisinger <gmeisinger@gmail.com>
2019-08-25 14:28:39 -0500

    added player sprite sheet and animations

---
+++ assets/lyre_of_truth.tsx
@@ -0,0 +1,116 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="lyre_of_truth" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="tileset.png" width="320" height="320"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 32,0 32,32 0,32"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 32,0 32,32 0,32"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index">
+   <object id="1" x="8" y="32">
+    <polygon points="0,0 0,-21 4,-24 24,-24 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="8">
+    <polygon points="0,0 20,0 24,3 24,24 0,24"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 16,0 16,16 32,16 32,32 0,32"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index">
+   <object id="1" x="16" y="0">
+    <polygon points="0,0 16,0 16,32 -16,32 -16,16 0,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 32,0 32,16 16,16 16,32 0,32"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 32,0 32,32 16,32 16,16 0,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index">
+   <object id="1" x="8" y="8">
+    <polygon points="0,0 24,0 24,24 0,24"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="8">
+    <polygon points="0,0 24,0 24,24 0,24"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 32,0 32,32 0,32"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 32,0 32,32 0,32"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index">
+   <object id="1" x="8" y="0">
+    <polygon points="0,0 0,21 1,23 3,24 24,24 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 24,0 24,21 23,23 21,24 0,24"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup draworder="index">
+   <object id="1" x="8" y="0">
+    <polygon points="0,0 0,5 1,7 3,8 24,8 24,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 24,0 24,5 23,7 21,8 0,8"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
