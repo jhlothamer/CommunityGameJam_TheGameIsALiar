de4e243a495a560271c714a96eb85a1da7566e6a
gmeisinger <gmeisinger@gmail.com>
2019-08-28 19:53:16 -0500

    adjusted map script, tested with new level

--- game/scenes/levels/level.gd
+++ game/scenes/levels/level.gd
@@ -4,4 +4,5 @@ func _ready():
 	var map_depth = $tileMapPrepper.get_level_bottom()
 	var camera = $player/Camera2D
 	camera.limit_bottom = map_depth
-	$ParallaxBackground.scroll_limit_end.y = map_depth / camera.zoom.y
+	print(camera.limit_bottom)
+	$ParallaxBackground.scroll_limit_end.y = (camera.limit_bottom - 16.0) / camera.zoom.y
