7e2aeeac6fe634e64d67baf8ad51dd33913c3d29
gmeisinger <gmeisinger@gmail.com>
2019-08-30 12:42:43 -0500

    added 4th level, slightly adjusted current levels

--- game/scenes/util/LevelMgr.gd
+++ game/scenes/util/LevelMgr.gd
@@ -4,8 +4,9 @@ var current_level_index = -1
 
 var levels = [
 	"res://scenes/levels/level_1.tscn",
-	"res://scenes/levels/level_2.tscn",
-	"res://scenes/levels/level_3.tscn"
+	"res://scenes/levels/level_4.tscn",
+	"res://scenes/levels/level_3.tscn",
+	"res://scenes/levels/level_2.tscn"
 ]
 
 func _ready():
@@ -15,5 +16,6 @@ func on_NextLevel():
 	current_level_index += 1
 	if current_level_index >= levels.size():
 		get_tree().change_scene("res://scenes/credits/credits.tscn")
+		current_level_index = 1
 	else:
 		get_tree().change_scene(levels[current_level_index])
